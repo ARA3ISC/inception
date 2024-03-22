cf49eb6becd9cc494ecb928fef6f30c37bfbb000
arabisco <m.aneddame01@gmail.com>
2024-03-22 04:14:36 +0000

    .

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,20 +1,12 @@
 #! /bin/bash
 
 service mariadb start
-# sleep 2
+sleep 2
 
-# echo "db_user hhhhhhhhhh : $hostdb_username"
-# echo "host name : $hostname"
-# echo "db : $db"
+mariadb -e "CREATE USER '$db_user'@'$hostname' IDENTIFIED BY '$db_password';"
+mariadb -e "CREATE DATABASE $db;"
+mariadb -e "GRANT ALL PRIVILEGES ON $db.* to '$db_user'@'$hostname' IDENTIFIED BY '$db_password';"
+mariadb -e "FLUSH PRIVILEGES;"
 
-# mariadb -e "CREATE USER '$db_user'@'$hostname' IDENTIFIED BY '$db_password';"
-# mariadb -e "CREATE DATABASE $db;"
-# mariadb -e "GRANT ALL PRIVILEGES ON $db.* to '$db_user'@'$hostname' IDENTIFIED BY '$db_password';"
-
-# mariadb -e "FLUSH PRIVILEGES;"
-
-
-# service mariadb stop
-# service mariadb start
-# exec mysqld_safe
-# bash
+service mariadb stop
+exec mysqld_safe
--- srcs/requirements/nginx/tools/ngx_script.sh
+++ srcs/requirements/nginx/tools/ngx_script.sh
@@ -1,9 +1,10 @@
 #! bin/bash
 
 
-sed -i "s/^;pm.status_listen = 127.0.0.1:9001/listen=9000/" /etc/php/7.4/fpm/pool.d/www.conf
-ln -s /etc/nginx/sites-available/wp.conf /etc/nginx/sites-enabled/
+# sed -i "s/^;pm.status_listen = 127.0.0.1:9000/listen=9000/" /etc/php/7.4/fpm/pool.d/www.conf
+# ln -s /etc/nginx/sites-available/wp.conf /etc/nginx/sites-enabled/
 
-service nginx reload
+# service nginx reload
 
-nginx -g daemon off;
+nginx -g "daemon off;"
+

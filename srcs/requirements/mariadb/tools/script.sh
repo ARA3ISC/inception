4f2def00f64baee5b336316a17ef6d8e8c31b78f
arabisco <m.aneddame01@gmail.com>
2024-03-23 00:57:50 +0000

    .

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -3,7 +3,7 @@
 service mariadb start
 sleep 2
 
-mariadb -e "CREATE USER '$db_user'@'$%' IDENTIFIED BY '$db_password';"
+mariadb -e "CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_password';"
 mariadb -e "CREATE DATABASE $db;"
 mariadb -e "GRANT ALL PRIVILEGES ON $db.* to '$db_user'@'%' IDENTIFIED BY '$db_password';"
 mariadb -e "FLUSH PRIVILEGES;"

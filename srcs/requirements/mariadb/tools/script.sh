77a58bb678660344cb607f778f529faec84d97c9
ara3isc <m.aneddame01@gmail.com>
2024-04-21 16:30:20 +0100

    before testing makefile

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -7,10 +7,6 @@ mariadb -e "CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_password';"
 mariadb -e "CREATE DATABASE $db;"
 mariadb -e "GRANT ALL PRIVILEGES ON $db.* to '$db_user'@'%' IDENTIFIED BY '$db_password';"
 
-mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'wordpress.srcs_mynetwork' IDENTIFIED BY '1234' WITH GRANT OPTION;"
-
-
-
 mariadb -e "FLUSH PRIVILEGES;"
 
 service mariadb stop
